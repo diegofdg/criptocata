cb2ba9f79fde384b8d94ac3bdf3f1d22bf118c53
diegofdg <diegofdg@gmail.com>
2022-01-23 16:18:28 -0300

    agregando el formulario reactivo de iniciar sesión

--- src/app/pages/iniciar-sesion/iniciar-sesion.component.ts
+++ src/app/pages/iniciar-sesion/iniciar-sesion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-iniciar-sesion',
@@ -6,10 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./iniciar-sesion.component.css']
 })
 export class IniciarSesionComponent implements OnInit {
+  form:FormGroup = new FormGroup({});
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.form= this.formBuilder.group(
+      {
+        password:['', [Validators.required, Validators.minLength(8)]],
+        mail:['', [Validators.required, Validators.email]]
+      }
+    )
   }
 
+  get Password() {
+    return this.form.get("password");
+  }
+  
+  get Mail() {
+   return this.form.get("mail");
+  }
+
+  get PasswordValid() {
+    return this.Password?.touched && !this.Password?.valid;
+  }
+
+  get MailValid() {
+    return this.Mail?.touched && !this.Mail?.valid;
+  } 
+
+  ngOnInit(): void {
+       
+  }
+
+  onEnviar(event: Event) {
+    event.preventDefault;
+    if (this.form.valid) {
+      alert ("Enviar al servidor...")
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -14,6 +14,7 @@ import { CriptomonedaComponent } from './criptomoneda/criptomoneda.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { RouterModule } from '@angular/router';
 import { MovimientosComponent } from './movimientos/movimientos.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -33,7 +34,8 @@ import { MovimientosComponent } from './movimientos/movimientos.component';
   imports: [
     CommonModule,
     AppRoutingModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule
   ],
   exports: [
     ServiciosComponent
